5eb4ba1e61fe78e286bbe79dadff975009867d67
github username <github email address>
2019-06-18 13:52:50 -0700

    Continuing work on user login/creation.

--- lib/Models/user_interaction.rb
+++ lib/Models/user_interaction.rb
@@ -9,10 +9,26 @@ class UserInteraction
   end
 
   def gets_input
-    puts "Please enter your name:"
-    name = gets.chomp
-    
+    prompt = "Type 1) to enter your pin or type or 2) to create a new account."
+    puts prompt
+    selection = gets.chomp.to_i
+    if selection == 1
+      puts "Please enter your pin:"
+      while pin = gets.chomp.to_i
+        case pin
+        when @user.pin == pin
+          puts "Welcome back, #{@user.name}!"
+          break
+        when @user.pin != pin
+          puts "Incorrect pin."
+          break
+        else
+          puts prompt
+        end
+
   end
+
+
 
 
 end
