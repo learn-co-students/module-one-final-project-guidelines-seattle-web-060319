eaf22da3b99b7ebdeef8659b4bd1e5d9972465e7
Spencer Lindemuth <lindemut@gmail.com>
2019-06-18 10:26:57 -0700

    Actually got a migration to run!

--- db/Migrate/001_create_stocks.rb
+++ db/Migrate/001_create_stocks.rb
@@ -1,4 +1,4 @@
-class CreateStock < ActiveRecord[5.2]
+class CreateStocks < ActiveRecord::Migration[5.0]
     def change
         create_table :stocks do |t|
             t.string :name
@@ -6,5 +6,6 @@ class CreateStock < ActiveRecord[5.2]
             t.float :price
             t.float :open_price
             t.float :previous_close_price
+        end
     end
 end
