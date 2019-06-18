6ce796a1d85436b370da7ea1820f8d50e304fe90
Spencer Lindemuth <lindemut@gmail.com>
2019-06-18 11:46:17 -0700

    Added new columns to stocks table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 3) do
+ActiveRecord::Schema.define(version: 4) do
 
   create_table "portfolios", force: :cascade do |t|
     t.integer "user_id"
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 3) do
     t.float  "price"
     t.float  "open_price"
     t.float  "previous_close_price"
+    t.float  "changes"
+    t.string "sector"
   end
 
   create_table "users", force: :cascade do |t|
