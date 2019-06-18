06882490e8f23086173f501a2ef81079093ff8ab
Spencer Lindemuth <lindemut@gmail.com>
2019-06-17 17:28:30 -0700

    Implement similar text comparison

--- lib/test.rb
+++ lib/test.rb
@@ -1,5 +1,24 @@
-puts "Hello Aliya"
+require 'rest-client'
+require 'pry'
+require 'similar_text'
 
-puts "hello spencer"
+url = "https://financialmodelingprep.com/api/v3/stock/real-time-price"
+response_string = RestClient.get(url)
+response_hash = JSON.parse(response_string)
 
-puts "testing git control in VScode"
+
+
+def find_price_by_symbol(symbol, hash)
+
+    aapl = hash["stockList"].select do |stock|
+        stock["symbol"] == symbol
+    end
+end
+
+def find_similarity_by_symbol(hash, symbol)
+    hash["stockList"].select do |stock|
+        stock["symbol"].similar(symbol) >= 75.0
+    end
+end
+
+Pry.start
