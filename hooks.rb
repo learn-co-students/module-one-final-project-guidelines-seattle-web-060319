e87dd5c021b2dc4f17d8ea2addad50c4c667fb00
Spencer Lindemuth <lindemut@gmail.com>
2019-06-18 15:50:45 -0700

    Updated with validation hook

--- hooks.rb
+++ hooks.rb
@@ -1,5 +1,8 @@
 #Returns an array up to 5 elements of the closest matches to the entered name
     StockByName.match_name(NAME_APPROXIMATION)
 
+#Return TRUE or FALSE depending on whether the input was a valid stock symbol
+    valid_symbol?(SYMBOL)
+
 #Returns the stock object associated with the stock symbol given after adding it to the database
     StockFromSymbol.new(STOCK_SYMBOL)
