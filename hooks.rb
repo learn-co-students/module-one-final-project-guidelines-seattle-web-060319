50e9dc29ec66ad5aa3461485cc0a184864f2beab
Spencer Lindemuth <lindemut@gmail.com>
2019-06-18 15:35:40 -0700

    Added hooks directory

---
+++ hooks.rb
@@ -0,0 +1,5 @@
+#Returns an array up to 5 elements of the closest matches to the entered name
+    StockByName.match_name(NAME_APPROXIMATION)
+
+#Returns the stock object associated with the stock symbol given after adding it to the database
+    StockFromSymbol.new(STOCK_SYMBOL)
