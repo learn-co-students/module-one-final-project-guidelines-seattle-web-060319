68fbc97012baad25f1a1deaebd87bd704e1e601b
github username <github email address>
2019-06-18 11:35:19 -0700

    imaginary changes

--- lib/Models/stock.rb
+++ lib/Models/stock.rb
@@ -1,9 +1,5 @@
 class Stock < ActiveRecord::Base
-<<<<<<< HEAD
-    has_many :users, through: :portfolio
-=======
     has_many :portfolios
     has_many :users, through: :portfolios
-    
->>>>>>> 434e0a462a5e797541089690098d0fac80d31b28
+
 end
