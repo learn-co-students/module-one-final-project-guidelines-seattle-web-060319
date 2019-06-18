dec36e3cebc8a9466638ecae85a4a7d41e43bce9
Spencer Lindemuth <lindemut@gmail.com>
2019-06-18 14:52:57 -0700

    Wrote search function for stock name

---
+++ lib/Models/stock_by_name.rb
@@ -0,0 +1,50 @@
+class StockByName
+
+
+    #compares the name to the full company list, and returns top 5 companies with closest name match
+    def initialize(name)
+        @name = name
+    end
+
+    def self.company_list
+        url = "https://financialmodelingprep.com/api/v3/company/stock/list"
+        response_string = RestClient.get(url)
+        response_hash = JSON.parse(response_string)
+    end
+
+    def self.match_name(name)
+        name = name.titleize
+        return_hash = {}
+        company_list["symbolsList"].each do |company|
+            if company["name"].include?(name)
+                return_hash[company["symbol"]] = company["name"]
+            elsif company["name"].similar(name) > 50.0
+                return_hash[company["symbol"]] = company["name"]
+            elsif company["symbol"].similar(name.upcase) > 75.0
+                return_hash[company["symbol"]] = company["name"]  
+            end
+
+        end
+        trim(return_hash)
+    end
+
+    #trims the hash to a 5 element array
+    def self.trim(hash)
+        return_array = []
+        if hash.count >= 5
+            5.times do 
+                return_array << hash.shift
+            end
+        else
+            hash.count.times do
+                return_array << hash.shift
+            end
+        end
+        return_array
+    end
+
+
+
+
+end
+            
