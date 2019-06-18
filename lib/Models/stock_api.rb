043cb26fec6af4b7992830dd95d05fecf2a9a582
Spencer Lindemuth <lindemut@gmail.com>
2019-06-18 14:15:49 -0700

    Wrote hook for stock by symbol

--- lib/Models/stock_api.rb
+++ lib/Models/stock_api.rb
@@ -1,22 +1,11 @@
 class StockFromSymbol
 
-    attr_reader :symbol
+    attr_reader :symbol, :price, :name, :day_change, :sector, :open_price
 
     def initialize(symbol)
         @symbol = symbol.upcase
-        url = "https://financialmodelingprep.com/api/v3/company/profile/#{@symbol}"
-        response_string = RestClient.get(url)
-        response_hash = JSON.parse(response_string)
-        binding.pry
-        if response_hash.has_key?("Error")
-            return "#{@symbol} is an invalid Symbol"
-        else
-            @price = response_hash["profile"]["price"]
-            @name = response_hash["profile"]["companyName"]
-            @changes = response_hash["profile"]["changes"]
-            @sector = response_hash["profile"]["sector"]
-        end
-        Stock.create(name: @name, symbol: @symbol, price: @price, changes: @changes, sector: @sector)
+        response_hash = stock_profile(symbol)
+        Stock.create(name: @name, symbol: @symbol, price: @price, open_price: @open_price, day_change: @day_change, sector: @sector)
     end
 
 
@@ -26,11 +15,28 @@ class StockFromSymbol
         url = "https://financialmodelingprep.com/api/v3/company/profile/#{@symbol}"
         response_string = RestClient.get(url)
         response_hash = JSON.parse(response_string)
+        if response_hash.has_key?("Error")
+            return "#{@symbol} is an invalid Symbol"
+        else
+            @price = response_hash["profile"]["price"]
+            @name = response_hash["profile"]["companyName"]
+            @day_change = response_hash["profile"]["changes"]
+            @sector = response_hash["profile"]["sector"]
+            @open_price = get_open_price(symbol)
+        end
     end
 
-    def other_profile
+    def get_open_price(symbol)
+        current_date = DateTime.now
+        current_date.strftime "%Y-%m-%d".to_s
+        yesterday_date = Date.today.prev_day
+        yesterday_date.strftime "%Y-%m-%d".to_s
+        url = "https://financialmodelingprep.com/api/v3/historical-price-full/#{symbol}?from=#{yesterday_date}to=#{current_date}"
+        response_string = RestClient.get(url)
+        response_hash = JSON.parse(response_string)
+        response_hash["historical"][0]["open"]
+    end
 
-        https://financialmodelingprep.com/api/v3/historical-price-full/AAPL?from=2018-03-12to=2019-03-12
 
 
 
