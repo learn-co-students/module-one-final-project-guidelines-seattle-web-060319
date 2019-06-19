939f790f2d05e757185641ceea6927d2576452ef
Spencer Lindemuth <lindemut@gmail.com>
2019-06-19 11:05:03 -0700

    Add functionality to update stocks

--- lib/Models/stock_from_symbol.rb
+++ lib/Models/stock_from_symbol.rb
@@ -1,24 +1,35 @@
     def StockBySymbol(symbol)
-        @symbol = symbol.upcase
-        response_hash = stock_profile(symbol)
-        Stock.create(name: @name, symbol: @symbol, price: @price, open_price: @open_price, day_change: @day_change, sector: @sector)
+        symbol = symbol.upcase
+        stock_attributes(symbol)
     end
 
 
 
 
-    def stock_profile(symbol)
-        url = "https://financialmodelingprep.com/api/v3/company/profile/#{@symbol}"
+    def stock_attributes(symbol)
+        url = "https://financialmodelingprep.com/api/v3/company/profile/#{symbol}"
         response_string = RestClient.get(url)
         response_hash = JSON.parse(response_string)
         if response_hash.has_key?("Error")
-            return "#{@symbol} is an invalid Symbol"
+            return "#{symbol} is an invalid Symbol"
         else
-            @price = response_hash["profile"]["price"]
-            @name = response_hash["profile"]["companyName"]
-            @day_change = response_hash["profile"]["changes"]
-            @sector = response_hash["profile"]["sector"]
-            @open_price = get_open_price(symbol)
+            price = response_hash["profile"]["price"]
+            name = response_hash["profile"]["companyName"]
+            day_change = response_hash["profile"]["changes"]
+            sector = response_hash["profile"]["sector"]
+            open_price = get_open_price(symbol)
+        end
+        if Stock.exists?(symbol: symbol)
+            stock = Stock.find_by symbol: symbol
+            stock.price = price
+            stock.day_change = day_change
+            stock.open_price = open_price
+            stock.save
+            return stock
+        elsif valid_symbol?(symbol)
+            return Stock.create(name: name, symbol: symbol, price: price, open_price: open_price, day_change: day_change, sector: sector)
+        else
+            return "Invalid symbol: #{symbol}"
         end
     end
 
