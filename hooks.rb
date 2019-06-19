8dca4c4a35994fd0eb15fb855e933cc52110f928
Spencer Lindemuth <lindemut@gmail.com>
2019-06-19 09:28:23 -0700

    Added new hooks and updated ByName and BySymbol

--- hooks.rb
+++ hooks.rb
@@ -1,11 +1,11 @@
 #Returns an array up to 5 elements of the closest matches to the entered name
-    StockByName.match_name(NAME_APPROXIMATION)
+    StockByName(NAME_APPROXIMATION)
 
 #Return TRUE or FALSE depending on whether the input was a valid stock symbol
     valid_symbol?(SYMBOL)
 
 #Returns the stock object associated with the stock symbol given after adding it to the database
-    StockFromSymbol.new(STOCK_SYMBOL)
+    StockBySymbol(STOCK_SYMBOL)
 
 #Returns an array of the 10 biggest gainers of the day
     biggest_positive_movers()
@@ -16,3 +16,9 @@
 #Returns hash of format below, after giving stock symbol, and number of days to go back historically
 #{"first_close"=>OLDEST DATE CLOSE VALUE, "current_close"=>CURRENT DATE CLOSE VALUE, "total_change"=> FLOAT CHANGE, "change_percent"=> FLOAT CHANGE * 100 FOR PERCENT, "change_direction"=> "UP" OR "DOWN" DEPENDING ON POSITVE GROWTH OR NEGATIVE GROWTH}
     stock_history(symbol, num_days)
+
+#Returns TRUE if market is currently open, FALSE if the market is currently closed.
+    market_open?
+
+#Takes in a user object, and iterates through each of the stocks in it's portfolio updating their current price
+    update_portfolio_prices(USER)
